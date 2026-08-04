c4b7b9ddd5baf905664a96bf7baefb3385037885
vanellie-j <boboliak@gmail.com>
2026-08-04 20:21:16 +0000

    Refactor argument logging to use inspectArguments function for better clarity

--- meshcentral_bridge.js
+++ meshcentral_bridge.js
@@ -1,16 +1,39 @@
 module.exports.meshcentral_bridge = function (parent) {
     const obj = {};
 
+    function inspectArguments(hookName, args) {
+        const summary = [];
+
+        for (let i = 0; i < args.length; i++) {
+            const value = args[i];
+
+            summary.push({
+                index: i,
+                type: typeof value,
+                constructor: value?.constructor?.name ?? null,
+                keys: (
+                    value !== null &&
+                    typeof value === 'object'
+                ) ? Object.keys(value).slice(0, 30) : null
+            });
+        }
+
+        console.log(
+            `[meshcentral_bridge] ${hookName}`,
+            JSON.stringify(summary)
+        );
+    }
+
     obj.server_startup = function () {
         console.log('[meshcentral_bridge] server_startup');
     };
 
     obj.hook_agentCoreIsStable = function () {
-        console.log('[meshcentral_bridge] hook_agentCoreIsStable', arguments);
+        inspectArguments('hook_agentCoreIsStable', arguments);
     };
 
     obj.hook_processAgentData = function () {
-        console.log('[meshcentral_bridge] hook_processAgentData', arguments);
+        inspectArguments('hook_processAgentData', arguments);
     };
 
     return obj;
