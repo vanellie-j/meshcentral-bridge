bce607934b2fb8f301cb16b7ada5da467514c7aa
vanellie-j <boboliak@gmail.com>
2026-08-04 20:06:07 +0000

    Rename plugin references from Pattern Bridge to Meshcentral Bridge and update URLs in config.json

--- meshcentral_bridge.js
+++ meshcentral_bridge.js
@@ -1,16 +1,16 @@
-module.exports.pattern_bridge = function (parent) {
+module.exports.meshcentral_bridge = function (parent) {
     const obj = {};
 
     obj.server_startup = function () {
-        console.log('[pattern_bridge] server_startup');
+        console.log('[meshcentral_bridge] server_startup');
     };
 
     obj.hook_agentCoreIsStable = function () {
-        console.log('[pattern_bridge] hook_agentCoreIsStable', arguments);
+        console.log('[meshcentral_bridge] hook_agentCoreIsStable', arguments);
     };
 
     obj.hook_processAgentData = function () {
-        console.log('[pattern_bridge] hook_processAgentData', arguments);
+        console.log('[meshcentral_bridge] hook_processAgentData', arguments);
     };
 
     return obj;
